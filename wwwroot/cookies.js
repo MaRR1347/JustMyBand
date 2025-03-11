fbe7fc3861443f7bfdb8602f94576913177a3d7f
JakubRams <ramsjakub01@gmail.com>
2025-03-12 00:10:51 +0100

    jd

---
+++ wwwroot/cookies.js
@@ -0,0 +1,20 @@
+﻿window.getCookie = (name) => {
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i].trim();
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+};
+
+
+window.setCookie = (name, value, days) => {
+    let expires = "";
+    if (days) {
+        let date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+};
